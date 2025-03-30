f4e63cca6440cc83e683fd457959f203b6ca0927
AlecCross <cross2472@gmail.com>
2025-03-30 15:16:15 +0000

    fix icon for tab

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
        <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/icon-16.ico" />
+          <link rel="icon" type="image/webp" sizes="32x32" href="/icon-32.webp" />
+          <link rel="icon" type="image/webp" sizes="16x16" href="/icon-16.webp" />
           <link rel="manifest" href="/manifest.json" />
         </Head>
       <body>
